163f331c058d77b033770755e405741be1075e42
Jeremy Southon <southon55@gmail.com>
2019-04-30 19:29:56 +1200

    about to update schema to add Item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const Mutations = {
 
-module.exports = mutations;
+    createUser(parent,args,context,info) {
+        global.users = global.users || [];
+        const user = {name:args.name}
+        global.users.push(user);
+        console.log(global.users);
+    }
+};
+
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+// Query resolvers
+const Query = {
+    users(parent,arg,context,info){
+        return [{name:'jeremy'}, {name:'dan'}]
+    }
+
+
+};
+
+
 
 module.exports = Query;
